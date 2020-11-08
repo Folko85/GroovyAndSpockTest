e384b4b35b9823178a411f29bbe4226b55fa1700
Othernik <folko85@yandex.ru>
2020-11-09 00:16:54 +0300

    Groovy and Spock. More pretty view with WHERE and EXPECT

--- src/test/groovy/AuthSpecification.groovy
+++ src/test/groovy/AuthSpecification.groovy
@@ -2,88 +2,62 @@ import groovyx.net.http.HttpResponseException
 import groovyx.net.http.RESTClient
 import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class AuthSpecification extends Specification {
 
     @Shared def client = new RESTClient( "https://postman-echo.com/")
-    def SUCCESS_RESPONSE = 200
-    def UNAUTHORIZED_RESPONSE = 401
+    @Shared def SUCCESS_RESPONSE = 200
+    @Shared def UNAUTHORIZED_RESPONSE = 401
+    @Shared def basicProperty, digestProperty, oauth1Property, basic, digest, oauth1
 
-    def setupSpec() {   //не стал выносить сюда url, для разнообразия
-
-    }
-
-    def "success basic auth"() {
-        setup:                             //BDD - особенность Spock
-        def basic = "/basic-auth"
-        when:
-        def x = getResponseFrom(basic, "Basic cG9zdG1hbjpwYXNzd29yZA==")
-        then:
-        x == SUCCESS_RESPONSE
-    }
-
-    def "failure basic auth"() {
-        setup:
-        def basic = "/basic-auth"    //все эти дефы больше похожи на JS
-        when:
-        getResponseFrom(basic, null)
-        then:
-        HttpResponseException e = thrown(HttpResponseException)   // Для http-билдера 401 - экзепшен.
-        e.response.status == UNAUTHORIZED_RESPONSE
-    }
-
-    def "success digest auth"() {
-        setup:
-        def digest = "/digest-auth"
-        when:
-        def x = getResponseFrom(digest,"Digest username=\"postman\"," +
+    def setupSpec() {     //эти гигантские строки вынесем, чтоб выглядело красиво
+        basicProperty = "Basic cG9zdG1hbjpwYXNzd29yZA=="
+        digestProperty = "Digest username=\"postman\"," +
                 " realm=\"Users\"," +
                 " nonce=\"ni1LiL0O37PRRhofWdCLmwFsnEtH1lew\"," +
                 " uri=\"/digest-auth\"," +
                 " response=\"254679099562cf07df9b6f5d8d15db44\"," +
-                " opaque=\"\"")
-        then:
-        x == SUCCESS_RESPONSE
-    }
-
-    def "failure digest auth"() {
-        setup:
-        def digest = "/digest-auth"
-        when:
-        getResponseFrom(digest, null)
-        then:
-        HttpResponseException e = thrown(HttpResponseException)
-        e.response.status == UNAUTHORIZED_RESPONSE
-    }
-
-    def "success oauth"() {
-        setup:
-        def oauth1 = "/oauth1"
-        when:
-        def x = getResponseFrom(oauth1,"OAuth oauth_consumer_key=\"RKCGzna7bv9YD57c\"," +
+                " opaque=\"\""
+        oauth1Property = "OAuth oauth_consumer_key=\"RKCGzna7bv9YD57c\"," +
                 "oauth_signature_method=\"PLAINTEXT\"," +
                 "oauth_timestamp=\"1602626369\"," +
                 "oauth_nonce=\"RohrSiqoTiH\"," +
                 "oauth_version=\"1.0\"," +
-                "oauth_signature=\"D%252BEdQ-gs%2524-%2525%25402Nu7%26\"")
-        then:
-        x == SUCCESS_RESPONSE
+                "oauth_signature=\"D%252BEdQ-gs%2524-%2525%25402Nu7%26\""
+        basic = "/basic-auth"
+        digest = "/digest-auth"
+        oauth1 = "/oauth1"
     }
 
-    def "failure oauth"() {
-        setup:
-        def oauth1 = "/oauth1"
+    @Unroll
+    def "auth success test"() {
+        expect:
+        getResponseFrom(requestType, request) == result
+        where:                                           //вот эта штука сильно сокращает казалось бы короткий код
+        requestType | request       || result
+        basic       | basicProperty || SUCCESS_RESPONSE
+        digest      | digestProperty|| SUCCESS_RESPONSE
+        oauth1      | oauth1Property|| SUCCESS_RESPONSE
+    }
+
+    @Unroll
+    def "auth failure test"() {
         when:
-        getResponseFrom(oauth1, null)
+        getResponseFrom(requestType, request)
         then:
         HttpResponseException e = thrown(HttpResponseException)
-        e.response.status == UNAUTHORIZED_RESPONSE
+        e.response.status == result
+        where:                               //вот эта штука сильно сокращает казалось бы короткий код
+        requestType | request       || result
+        basic       | null  || UNAUTHORIZED_RESPONSE
+        digest      | null  || UNAUTHORIZED_RESPONSE
+        oauth1      | null  || UNAUTHORIZED_RESPONSE
     }
-          //поскольку любой джава-код является верным груви-кодом, то просто оставим этот метод неизменным
+
     def getResponseFrom(String path, String property) throws IOException {
         client.headers['Authorization'] = property
         def response = client.get( path : path )
         return response.status
     }
-
 }
